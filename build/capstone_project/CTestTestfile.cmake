1193b11b69951c55f46c9a92dd1ade3856dbc3f9
parfait908 <parfaitngoi@gmail.com>
2024-12-15 11:59:59 +0200

    Add ROS workspace

---
+++ build/capstone_project/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/parfait/ciu_capstone/src/capstone_project
+# Build directory: /home/parfait/ciu_capstone/build/capstone_project
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/parfait/ciu_capstone/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/parfait/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/parfait/catkin_ws/devel;/opt/ros/noetic' != '' else []
